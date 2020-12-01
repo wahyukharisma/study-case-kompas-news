4f37c61881d6a5c4a87a54a28affbaaf9b676ba7
wahyukharisma <madewahyu39@gmail.com>
2020-12-01 16:44:29 +0700

    Initialize folder and info file
    + Adapter
    + View Model
    + Repository
    + Model
    + View
    + Network

---
+++ app/src/main/java/com/example/myapplication/network/NewsNetwork.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication.network
+
+interface NewsNetwork {
+}
---
+++ app/src/main/java/com/example/myapplication/network/model/DetailsArticle.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.network.model
+
+data class DetailsArticle(
+    val author: String,
+    val post_content: String,
+    val post_date: String,
+    val post_excerpt: String,
+    val post_title: String
+)
